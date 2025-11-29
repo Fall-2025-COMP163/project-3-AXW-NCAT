3b75f7252d59de7f3cfaffcfbcfb008efe6620cc
Aaron Williams <axwabq423@gmail.com>
2025-11-29 16:44:25 -0500

    Implement inventory full check in unequip methods
    Added inventory full check before unequipping weapons and armor.

--- inventory_system.py
+++ inventory_system.py
@@ -188,6 +188,8 @@ def unequip_weapon(character):
     Returns: Item ID that was unequipped, or None if no weapon equipped
     Raises: InventoryFullError if inventory is full
     """
+    if character["inventory"] > MAX_INVENTORY_SIZE:
+        raise InventoryFullError ("Inventory is at max compacity")
     # TODO: Implement weapon unequipping
     # Check if weapon is equipped
     # Remove stat bonuses
@@ -202,6 +204,8 @@ def unequip_armor(character):
     Returns: Item ID that was unequipped, or None if no armor equipped
     Raises: InventoryFullError if inventory is full
     """
+    if character["inventory"] > MAX_INVENTORY_SIZE:
+        raise InventoryFullError ("Inventory is at max compacity")
     # TODO: Implement armor unequipping
     pass
 
@@ -223,6 +227,8 @@ def purchase_item(character, item_id, item_data):
         InsufficientResourcesError if not enough gold
         InventoryFullError if inventory is full
     """
+    if character["inventory"] > MAX_INVENTORY_SIZE:
+        raise InventoryFullError ("Inventory is at max compacity")
     # TODO: Implement purchasing
     # Check if character has enough gold
     # Check if inventory has space
